a000b668a0b9963acc471d51303018711a48aeb3
kefarova-png <kefarova@gmail.com>
2026-08-11 02:15:48 +0400

    Version 2. Вебдрайвер внесён в __init__ класса

--- Selenium12_Test_Class.py
+++ Selenium12_Test_Class.py
@@ -4,37 +4,29 @@ from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+#  from selenium.webdriver.common.by import By
 
 
-#  Chrome. Добавляем опции браузера: detach, True -- чтобы Chrome не закрывал окно браузера после завершения работы кода
-webdriver.ChromeOptions().add_experimental_option("detach",True)
-#  Создаём вебдрайвер Chrome c этими опциями и с автоматической проверкой/установкой драйвера
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()),
-    options=webdriver.ChromeOptions()
-)
+class Open_by_Chrome:
+    def __init__(self, detach=True):  #  создание вебдрайвера Chrome
+        options = webdriver.ChromeOptions()  #  настройки Chrome
+        options.add_experimental_option("detach", True)  #  в настройки добавлен параметр detach
+        print(f'Chrome experimental options = {options._experimental_options}\n')  #  сообщение об опциях Chrome
+        self.driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),  #  вебдрайвер ChromeDriver скачан/запущен автоматически
+            options=options  #  ранее созданные опции вебдрайвера
+        )
 
-class Test():
-    def product_select(self):  #  открытие сайта в окне
+    def opening_the_site(self):  #  открытие сайта в окне браузера
         #  Открываем вебдрайвером ссылку
-        driver.get('https://saucedemo.com/')
+        self.driver.get('https://saucedemo.com/')
         #  Устанавливаем размер окна
-        driver.set_window_size(1080,1080)
-
-    def random_name_enter(self):  #  ввод случайного имени
-        #  Генерируем случайное имя
-        fake_name = Faker("en_US").first_name()  #  устанавливаем язык генерации -- английский США
-        print(f'A random name "{fake_name}" has been generated for use as a login')
-
-        #  Находим элемент для логина, используя XPATH, и вводим в поле логина сгенерированное случайное имя
-        driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys(fake_name)
-        print(f'Login "{fake_name}" entered')
+        self.driver.set_window_size(1080,1080)
+        print('The link is open in a Chrome window')
 
 
-test_start = Test()  #  экземпляр класса
-test_start.product_select()  #  открытие сайта в окне
-test_start.random_name_enter()  #  ввод случайного имени
+test_start = Open_by_Chrome()  #  экземпляр класса
+test_start.opening_the_site()  #  открытие сайта в окне браузера
 
 time.sleep(6)
-# driver.close()  #  Закрываем браузер
+test_start.driver.close()  #  закрываем браузер
